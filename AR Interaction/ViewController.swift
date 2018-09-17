faa3da9b6fd8819287064a912c6b9238d8275964
eryshkov <wendor265>
2018-09-17 22:17:07 +0300

    fix issues

--- AR Interaction/ViewController.swift
+++ AR Interaction/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/hoop.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -35,6 +35,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .vertical
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -46,17 +47,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    func addHoop(result: ARHitTestResult) {
+        let hoopScene = SCNScene(named: "art.scnassets/hoop.scn")!
+        
+        guard let hoopNode = hoopScene.rootNode.childNode(withName: "Hoop", recursively: false) else { return }
+        
+        let position = result.worldTransform.columns.3
+        
+        hoopNode.position = SCNVector3(position.x, position.y, position.z)
+        
+        sceneView.scene.rootNode.addChildNode(hoopNode)
+    }
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        //        print(#function, planeAnchor)
+        
+        let floor = createWall(planeAnchor: planeAnchor)
+        node.addChildNode(floor)
+        
     }
-*/
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor,
+            let floor = node.childNodes.first,
+            let geometry = floor.geometry as? SCNPlane
+            else { return }
+        geometry.width = CGFloat(planeAnchor.extent.x)
+        geometry.height = CGFloat(planeAnchor.extent.z)
+        
+        floor.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -72,4 +98,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    func createWall(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        
+        let geometry = SCNPlane(width: width, height: height)
+        
+        let node = SCNNode()
+        node.geometry = geometry
+        
+        node.opacity = 0.25
+        node.eulerAngles.x = -Float.pi / 2
+        
+        return node
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func screenTapped(_ sender: UITapGestureRecognizer) {
+        let touchLocation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchLocation, types: [.existingPlaneUsingExtent])
+        
+        if let result = hitTestResult.first {
+//            print("Пересеклись с поверхностью")
+            addHoop(result: result)
+        }
+    }
+    
 }
